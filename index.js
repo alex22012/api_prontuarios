c3d9d5b85d686cd47cdf7ba29dc3a15bc298e88b
alex22012 <alex.bonetti@amo.delivery>
2021-12-28 11:02:10 -0300

    finished auth route

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = require('./src/config/config')()
-app.listen(process.env.PORT || 3000)
+app.listen(process.env.PORT || 8080)
 
 app.get("/", (req, res) => {
     res.send("Oi")
--- src/config/config.js
+++ src/config/config.js
@@ -1,11 +1,12 @@
 const express = require("express")
 const cors = require("cors")
+const UserRoute = require("../routes/User")
 
 module.exports = () => {
     const app = express()
     app.use(express.urlencoded({extended:false}))
     app.use(express.json())
     app.use(cors())
-
+    app.use(UserRoute)
     return app
 }
